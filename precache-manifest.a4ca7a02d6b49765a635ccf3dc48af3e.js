66d09e7e979872b8566d50a0a0774092c2d3b726
Kirill Iluschenko <k.iluschenko@gmail.com>
2018-12-04 21:55:24 +0300

    Updates

---
+++ precache-manifest.a4ca7a02d6b49765a635ccf3dc48af3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/CoinCap/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "cfa21313d0ffa88365a2",
+    "url": "/CoinCap/static/js/runtime~main.cfa21313.js"
+  },
+  {
+    "revision": "e0e7c9a3f4c855da080e",
+    "url": "/CoinCap/static/js/main.e0e7c9a3.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/CoinCap/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "e0e7c9a3f4c855da080e",
+    "url": "/CoinCap/static/css/main.aaf5e523.chunk.css"
+  },
+  {
+    "revision": "ece47ab15cf2db3808214f6e25f07657",
+    "url": "/CoinCap/index.html"
+  }
+];
